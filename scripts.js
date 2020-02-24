2e4e2bfaf5f3c571d60bebc7f0cc8df205c4bafc
leetheonly <lawrenceireri201@gmail.com>
2020-02-24 10:11:09 -0500

    add index.html

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function akanName() {
+    var dd = parseInt(document.getElementById("DD").value);
+    var mm = parseInt(document.getElementById("MM").value);
+    var yy = parseInt(document.getElementById("YY").value);
+    var femaleGender = document.getElementById("femaleGender");
+    var maleGender = document.getElementById("maleGender");
+}
