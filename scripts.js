e653e272adda3818c3f7a9a41c02d128a72e1ea0
leetheonly <lawrenceireri201@gmail.com>
2020-02-27 17:42:10 -0500

    add modified scripts.js

--- scripts.js
+++ scripts.js
@@ -49,6 +49,6 @@ var name = document.getElementById("name").value;
 var email = document.getElementById("email").value;
 var message = document.getElementById("message").value;
 if (name !== "" && email !== "" && message !== "");
-  alert("Hey " +   name + " " + "thank you for contacting us,Delani Studio has received your message");
+  alert("Hey " +   name + " " + "thank you for contacting us.Delani Studio has received your message");
 event.preventDefault();
 }
